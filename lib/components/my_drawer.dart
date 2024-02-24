15f4064709d4365904a870df8913dad6f3177898
paras <parasad612@gmail.com>
2024-02-24 16:06:12 +0100

    added settings navigation

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../pages/settings.dart';
+
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
@@ -7,6 +9,36 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          DrawerHeader(child: Center(
+            child: Icon(
+              Icons.music_note,
+              size: 40,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          )
+          ),
+
+          ListTile(
+            title: Text("H O M E"),
+            leading: Icon(Icons.home),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text("S E T T I N G S"),
+            leading: Icon(Icons.home),
+            onTap: () {
+              // close drawer
+              Navigator.pop(context);
+              // navigate to settings
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage(),
+              )
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
